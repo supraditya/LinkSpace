fa82e39e29c9c531a08605b80d0889e361db0f52
luke0malley <lukeom@umich.edu>
2023-07-06 14:10:47 -0400

    add: working invite links to groups

--- app/controllers/group_users_controller.rb
+++ app/controllers/group_users_controller.rb
@@ -15,9 +15,38 @@ class GroupUsersController < ApplicationController
     redirect_to root_path
   end
 
+  def new
+    @group_user = @group.group_users.new
+  end
+
+  def create
+    @user = User.find_by(email: group_user_params[:email])
+
+    if @user
+      @group_user = @group.group_users.new(user: @user, admin: group_user_params[:admin])
+      if @group_user.save
+        redirect_to @group
+      else
+        render :new
+      end
+    else
+      @user = User.invite!({ email: group_user_params[:email] }, current_user)
+      @group_user = @group.group_users.new(user: @user, admin: group_user_params[:admin])
+      if @group_user.save
+        redirect_to @group
+      else
+        render :new
+      end
+    end
+  end
+
   private
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
+
+  def group_user_params
+    params.require(:group_user).permit(:email, :admin)
   end
 end
--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -3,10 +3,10 @@ class GroupUser < ApplicationRecord
   belongs_to :user
 
   def email
-    User.find(self.user_id).email
+    self.user&.email
   end
 
   def uniqname
-    User.find(self.user_id).email.split("@")[0]
+    self.user&.email&.split("@")&.first
   end
 end
