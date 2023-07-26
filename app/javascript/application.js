90058ecb91158ac54d241f542e1e4dff4eac28fe
supraditya <aditya300100@gmail.com>
2023-07-26 14:05:10 -0400

    ADD: Paste from clipboard button for new link view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+const pasteButton = document.querySelector('#paste-button');
+
+pasteButton.addEventListener('click', async () => {
+    const text = await navigator.clipboard.readText()
+   try {
+     const text = await navigator.clipboard.readText()
+     document.querySelector('#link-field').value += text;
+     console.log('Text pasted.');
+   } catch (error) {
+     console.log('Failed to read clipboard');
+   }
+});
